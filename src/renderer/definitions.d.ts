ee26a8b0f934544cf1f82504e2f9cf5f6bf8d759
Gareth Jones <jones258@gmail.com>
2019-02-09 13:16:13 +1300

    Add module definitions for file types targeted by `file-loader`

--- src/renderer/definitions.d.ts
+++ src/renderer/definitions.d.ts
@@ -4,3 +4,35 @@
   this is where you put things like interfaces,
   that should only be used by the renderer process.
  */
+
+/**
+ * Module for `.png` files
+ */
+declare module '*.ico' {
+  const fileName: string;
+  export default fileName;
+}
+
+/**
+ * Module for `.png` files
+ */
+declare module '*.png' {
+  const fileName: string;
+  export default fileName;
+}
+
+/**
+ * Module for `.jpg` files
+ */
+declare module '*.jpg' {
+  const fileName: string;
+  export default fileName;
+}
+
+/**
+ * Module for `.svg` files
+ */
+declare module '*.svg' {
+  const fileName: string;
+  export default fileName;
+}
