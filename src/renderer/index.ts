6f32f75dcb53032cb8db4b7d53c610f873a49502
Gareth Jones <jones258@gmail.com>
2019-02-09 14:13:53 +1300

    Remove `fs` import & logging
    You shouldn't use node apis in the renderer process

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,9 +1,7 @@
 import puts, { putHelloSunshine } from '@shared/puts';
-import * as fs from 'fs';
 
 puts('GOODNIGHT, ORSON!');
 
 putHelloSunshine();
 
 console.log(document);
-console.log(fs.readdirSync('.'));
