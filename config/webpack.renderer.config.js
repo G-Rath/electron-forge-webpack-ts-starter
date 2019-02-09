fab767009510d8197adf0430b44cba45c22a23d6
Gareth Jones <jones258@gmail.com>
2019-02-09 13:15:36 +1300

    Add `file-loader` to `webpack` config for `renderer`

--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -2,6 +2,15 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.config');
 
 /** @type {webpack.Configuration} */
-const config = {};
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.(ico|png|jpg|svg)$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
 
 module.exports = merge(common, config);
