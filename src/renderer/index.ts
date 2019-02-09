59972da532bd72a288cf1c56925929441d6dce1a
Gareth Jones <jones258@gmail.com>
2019-02-09 14:21:02 +1300

    Adjust `renderer/index.ts` to set `src` of `img#my-image` element to `grey-room.jpg`

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,7 +1,14 @@
 import puts, { putHelloSunshine } from '@shared/puts';
+import jpgGreyRoom from '@assets/grey-room.jpg';
 
 puts('GOODNIGHT, ORSON!');
 
 putHelloSunshine();
 
 console.log(document);
+
+const img = document.getElementById('my-image');
+
+if (img instanceof HTMLImageElement) {
+  img.src = jpgGreyRoom;
+}
